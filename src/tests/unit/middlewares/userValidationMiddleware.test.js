bf309970f34a9f3b6cce81b7effd9a03632e5c7c
haidarmaula <haidarmaulareynaldy@gmail.com>
2024-12-06 16:29:05 +0700

    add unit tests for validateUserLogin middleware

--- src/tests/unit/middlewares/userValidationMiddleware.test.js
+++ src/tests/unit/middlewares/userValidationMiddleware.test.js
@@ -1,28 +1,23 @@
-jest.mock('bcrypt', () => ({
-  compare: jest.fn(),
-  hash: jest.fn(),
-}));
-
-jest.mock('../../../config/firestore', () => ({
-  collection: jest.fn().mockReturnThis(),
-  where: jest.fn().mockReturnThis(),
-  get: jest.fn(),
-  add: jest.fn(),
-}));
-
-const bcrypt = require('bcrypt');
-const db = require('../../../config/firestore');
-
 const {
   createMockRequest,
   createMockResponse,
   createMockNext,
-} = require('../utils/helper');
+} = require('../utils/jestMocks');
 
 const {
   validateUserRegistration,
   validateUserLogin,
 } = require('../../../middlewares/userValidationMiddleware');
+
+jest.mock('bcrypt');
+jest.mock('../../../config/firestore', () => ({
+  collection: jest.fn().mockReturnThis(),
+  where: jest.fn().mockReturnThis(),
+  get: jest.fn(),
+}));
+
+const bcrypt = require('bcrypt');
+const db = require('../../../config/firestore');
 
 describe('Validate user registration middleware', () => {
   let mockRequest;
@@ -30,7 +25,6 @@ describe('Validate user registration middleware', () => {
   let mockNext;
 
   beforeEach(() => {
-    console.log('bjir');
     mockRequest = createMockRequest({
       username: 'testUser',
       password: 'Tes@1234',
@@ -42,7 +36,7 @@ describe('Validate user registration middleware', () => {
   });
 
   it('Reject registration if any property is empty and return 400.', async () => {
-    mockRequest = createMockRequest({
+    const mockRequest = createMockRequest({
       username: '',
       password: 'Tes@1234',
       confirmPassword: 'Tes@1234',
@@ -96,6 +90,114 @@ describe('Validate user registration middleware', () => {
     });
 
     await validateUserRegistration(mockRequest, mockResponse, mockNext);
+
+    expect(mockResponse.status).not.toHaveBeenCalled();
+    expect(mockResponse.json).not.toHaveBeenCalled();
+    expect(mockNext).toHaveBeenCalled();
+  });
+});
+
+describe('Validate user login middleware', () => {
+  let mockRequest;
+  let mockResponse;
+  let mockNext;
+
+  beforeEach(() => {
+    mockRequest = createMockRequest({
+      username: 'testUser',
+      password: 'Tes@1234',
+    });
+    mockResponse = createMockResponse();
+    mockNext = createMockNext();
+  });
+
+  it('Reject login if any property is empty and return 400.', async () => {
+    const mockRequest = createMockRequest({
+      username: '',
+      password: 'Tes@1234',
+    });
+
+    await validateUserLogin(mockRequest, mockResponse, mockNext);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(400);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      status: 'error',
+      message: expect.any(String),
+      data: null,
+    });
+    expect(mockNext).not.toHaveBeenCalled();
+  });
+
+  it('Reject login if username does not exist and return 401.', async () => {
+    db.get.mockResolvedValueOnce({
+      empty: true,
+    });
+
+    await validateUserLogin(mockRequest, mockResponse, mockNext);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(401);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      status: 'error',
+      message: expect.any(String),
+      data: null,
+    });
+    expect(mockNext).not.toHaveBeenCalled();
+  });
+
+  it('Reject login for invalid password and return 401.', async () => {
+    db.get.mockResolvedValueOnce({
+      empty: false,
+      docs: [
+        {
+          data: () => ({
+            password: 'Tes@12345',
+          }),
+        },
+      ],
+    });
+
+    bcrypt.compare.mockResolvedValueOnce(false);
+
+    await validateUserLogin(mockRequest, mockResponse, mockNext);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(401);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      status: 'error',
+      message: expect.any(String),
+      data: null,
+    });
+    expect(mockNext).not.toHaveBeenCalled();
+  });
+
+  it('Reject login if server error and return 500.', async () => {
+    db.get.mockRejectedValueOnce(new Error('Database query failed.'));
+
+    await validateUserLogin(mockRequest, mockResponse, mockNext);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(500);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      status: 'error',
+      message: expect.any(String),
+      data: null,
+    });
+    expect(mockNext).not.toHaveBeenCalled();
+  });
+
+  it('Allow login if all conditions are met and return 200.', async () => {
+    db.get.mockResolvedValueOnce({
+      empty: false,
+      docs: [
+        {
+          data: () => ({
+            password: 'Tes@1234',
+          }),
+        },
+      ],
+    });
+
+    bcrypt.compare.mockResolvedValueOnce(true);
+
+    await validateUserLogin(mockRequest, mockResponse, mockNext);
 
     expect(mockResponse.status).not.toHaveBeenCalled();
     expect(mockResponse.json).not.toHaveBeenCalled();
