25a111972643e5f74bc6470e2ee7114875a1f148
haidarmaula <haidarmaulareynaldy@gmail.com>
2024-12-18 19:45:01 +0700

    add unit tests for updateUser controller

--- src/tests/unit/controllers/userController.test.js
+++ src/tests/unit/controllers/userController.test.js
@@ -5,7 +5,10 @@ const {
 
 const { createMockResponse } = require('../utils/jestMocks');
 
+jest.mock('bcrypt');
 jest.mock('../../../config/firestore', () => ({
+  collection: jest.fn().mockReturnThis(),
+  doc: jest.fn().mockReturnThis(),
   update: jest.fn(),
 }));
 
@@ -44,6 +47,50 @@ describe('Validate getUserById controller', () => {
         createdAt: 'some-date',
         updatedAt: 'some-date',
       },
+    });
+  });
+});
+
+describe('Validate updateUser controller', () => {
+  it('Fail to update user password when server encounters an error and return 500.', async () => {
+    const mockRequest = {
+      params: {
+        userId: 'my-userId',
+      },
+      body: {
+        newPassword: 'Tes@1234',
+      },
+    };
+
+    db.update.mockRejectedValueOnce(new Error('Database query failed.'));
+
+    await updateUser(mockRequest, mockResponse);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(500);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      status: 'error',
+      message: 'Failed to update password due to server error.',
+      data: null,
+    });
+  });
+
+  it('Successfully update user password and return 200.', async () => {
+    const mockRequest = {
+      params: {
+        userId: 'my-userId',
+      },
+      body: {
+        newPassword: 'Tes@1234',
+      },
+    };
+
+    await updateUser(mockRequest, mockResponse);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(200);
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      status: 'success',
+      message: 'Password updated successfully.',
+      data: null,
     });
   });
 });
